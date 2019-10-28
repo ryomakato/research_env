52a4c0a22c0d5a9f17f7f647eaea1ca9c6ba8115
Ryoma Kato <ryomakato@Ryomas-MacBook-Pro.local>
2019-10-28 09:11:52 +0900

    added docker-compose

--- make_env.sh
+++ make_env.sh
@@ -50,6 +50,10 @@ sudo apt-get install -y docker-ce
 sudo usermod -aG docker $USER
 sudo service docker start
 
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # install nvidia-docker if you want
 if [ ${GPU_OPTION:-false} == true ]; then
    # install nvidia-gpu-driver
